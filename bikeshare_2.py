8d4215fb72054e66c8a8f128410a9f2b9ee418a0
Luca Scarpantonio <20787210+scarpl@users.noreply.github.com>
2025-02-04 09:20:26 +0100

    Added Time to list of packages needed

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -1,4 +1,4 @@
-codeimport time
+import time
 import pandas as pd
 import numpy as np
 
@@ -16,7 +16,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    #get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
 
 
     # get user input for month (all, january, february, ... , june)
